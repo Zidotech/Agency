7927bf6e4620c3b1a42f583c65a22b9199a90934
Zulu <zulu.devs@gmail.com>
2023-10-15 10:11:44 +0100

    added new code

---
+++ src/utils/images.js
@@ -0,0 +1,9 @@
+import Hero_pic from "../assets/hero_pic.png";
+import logo from "../assets/logo.png";
+import star from "../assets/Star.png";
+
+export default {
+  Hero_pic,
+  logo,
+  star,
+};
--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,27 @@
 export default {
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        BodyBg: "rgba(40, 41, 62, 1)",
+        BodyBg2: "rgba(253, 240, 233, 1)",
+        Text_Light_Accent: "rgba(239, 109, 88, 1)",
+        Text_Dark_Accent: "rgba(239, 109, 88, 1)",
+        Text_Light: "rgba(255, 255, 255, 1)",
+        Text_Dark: "rgba(57, 20, 0, 1)",
+        Text_Light_Gray: "rgba(255, 255, 255, 0.64)",
+        Text_Dark_Gray: "rgba(57, 20, 0, 0.64)",
+        Border_Line: "rgba(243, 209, 191, 1)",
+      },
+    },
+    screens: {
+      xs: "480px",
+      ss: "620px",
+      sm: "768px",
+      md: "1060px",
+      lg: "1200px",
+      xl: "1700px",
+    },
   },
   plugins: [],
 };
